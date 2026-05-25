70753ea9f81eb5b9eb423bc5c411eb3c9c12687b
meyatech <surafelmengist2015@gmail.com>
2026-05-25 18:53:50 +0300

    add async/await

--- obj/Debug/net10.0/C#_project.AssemblyInfo.cs
+++ obj/Debug/net10.0/C#_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90a53704ac858a8cb71c6e1911464233645e4ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e05d92fe9cdd5330080dec0d2d692b619403e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
