cf31c9a7880f5f36543f05e615fc1a5e7232a8cf
meyatech <surafelmengist2015@gmail.com>
2026-05-25 17:01:16 +0300

    add initial commit .....

---
+++ Model.cs
@@ -0,0 +1,8 @@
+public class Student
+{
+    public string  Id;
+    public string Name;
+    public string Departement;
+    public decimal GPA;
+    public int Age;
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿List<Student> students = [
+    new Student{Id="s1",Name="Abel ",Departement="cs",GPA=2.67m,Age=21},
+    new Student{Id="s2",Name="selamawit ",Departement="software enginering",GPA=3.45m,Age=29},
+    new Student{Id="s3",Name="HENOK ",Departement="COMPUTER SCIENCE",GPA=3.05m,Age=45},
+    new Student{Id="s1",Name="Aster ",Departement="cs",GPA=3.85m,Age=27},
+];
+
+
+var studentInfo = students.OrderByDescending(c => c.Age);
+
+        // Display result
+        foreach (var student in studentInfo)
+        {
+            Console.WriteLine(
+                $"ID: {student.Id}, " +
+                $"Name: {student.Name}, " +
+                $"Department: {student.Departement}, " +
+                $"GPA: {student.GPA}, " +
+                $"Age: {student.Age}"
+            );
+        }
+    
